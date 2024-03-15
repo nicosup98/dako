93c022b3f8088de6edbe7ac58111a96f986308ec
null <null>
2024-03-15 18:36:03 -0400

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import db from '@astrojs/db';
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [db()],
+  integrations: [db(), tailwind()]
 });

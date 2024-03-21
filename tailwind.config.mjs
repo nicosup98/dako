da86c97251b9f9f9767363707eb702cc877553ce
nicolangelo damico <damiconicola98@gmail.com>
2024-03-20 23:01:04 -0400

    1.0

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	daisyui: {
+		themes: [
+			{
+				dako: {
+
+					"primary": "#7fffd4",
+
+					"secondary": "#d6d3d1",
+
+					"accent": "#6366f1",
+
+					"neutral": "#010b00",
+
+					"base-100": "#1f2937",
+
+					"info": "#00b9ff",
+
+					"success": "#34d399",
+
+					"warning": "#fde047",
+
+					"error": "#ff3471",
+				},
+			},
+		],
+	},
 	theme: {
 		extend: {},
 	},

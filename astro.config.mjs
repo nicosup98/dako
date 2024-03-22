6aa6b9a9a4b8071154f49093b016680059aed80a
nicosup98 <damiconicola98@gmail.com>
2024-03-22 15:30:03 -0400

    added svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import db from '@astrojs/db';
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), tailwind()]
+  integrations: [db(), tailwind(), svelte()],
+  output: "server",
+  adapter: vercel()
 });
